0edc55996c2e48621987650ea648549a1d8049f9
Haley Ristow <haleyhar44@yahoo.com>
2023-09-06 04:14:46 +0000

    fixed large files

---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>Haley Ristow</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello! My name is Haley Ristow. I am a Graphic Design and Web Development student. </p>
+        <p>
+          Here is a link to my Graphic Design 
+          <a href="https://www.behance.net/gallery/169087051/Portfolio"> Portfolio!</a>
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+  <Link href={`/posts/${id}`}>{title}</Link>
+  <br />
+  <small className={utilStyles.lightText}>
+    <Date dateString={date} />
+  </small>
+</li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+
+     
